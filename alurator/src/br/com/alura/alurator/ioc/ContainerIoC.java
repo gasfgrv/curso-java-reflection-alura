552b9abc29f0d1994659813d42db2b851896548c
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-08-16 21:59:21 -0300

    container de injeção de dependências

---
+++ alurator/src/br/com/alura/alurator/ioc/ContainerIoC.java
@@ -0,0 +1,41 @@
+package br.com.alura.alurator.ioc;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class ContainerIoC {
+
+	public Object getInstancia(Class<?> tipoFonte) {
+		Stream<Constructor<?>> construtores = Stream.of(tipoFonte.getDeclaredConstructors());
+
+		Optional<Constructor<?>> construtorPadrao = construtores
+				.filter(construtor -> construtor.getParameterCount() == 0).findFirst();
+
+		try {
+			if (construtorPadrao.isPresent()) {
+				Object instancia = construtorPadrao.get().newInstance();
+				return instancia;
+			} else {
+				Constructor<?> construtor = tipoFonte.getDeclaredConstructors()[0];
+
+				ArrayList<Object> params = new ArrayList<>();
+
+				for (Parameter param : construtor.getParameters()) {
+					Class<?> tipoDoParametro = param.getType();
+					params.add(getInstancia(tipoDoParametro));
+				}
+
+				return construtor.newInstance(params.toArray());
+			}
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| SecurityException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+}
